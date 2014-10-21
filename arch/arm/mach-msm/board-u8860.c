9e67fe016c3561e0c14ebc5ae89872d0743f77cb
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-10-21 14:35:37 +0300

    u8860: Allocate memory for 480x854 display.

--- arch/arm/mach-msm/board-u8860.c
+++ arch/arm/mach-msm/board-u8860.c
@@ -84,16 +84,16 @@
 
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
-#define MSM_FB_PRIM_BUF_SIZE	(800 * 480 * 4 * 3) /* 4bpp * 3 Pages */
+#define MSM_FB_PRIM_BUF_SIZE	(854 * 480 * 4 * 3) /* 4bpp * 3 Pages */
 #else
-#define MSM_FB_PRIM_BUF_SIZE	(800 * 480 * 4 * 2) /* 4bpp * 2 Pages */
+#define MSM_FB_PRIM_BUF_SIZE	(854 * 480 * 4 * 2) /* 4bpp * 2 Pages */
 #endif
 
 #define MSM_FB_EXT_BUF_SIZE	0
 
 #ifdef CONFIG_FB_MSM_OVERLAY0_WRITEBACK
 /* width x height x 3 bpp x 2 frame buffer */
-#define MSM_FB_OVERLAY0_WRITEBACK_SIZE	roundup((800 * 480 * 3 * 2), 4096)
+#define MSM_FB_OVERLAY0_WRITEBACK_SIZE	roundup((854 * 480 * 3 * 2), 4096)
 #else
 #define MSM_FB_OVERLAY0_WRITEBACK_SIZE	0
 #endif
